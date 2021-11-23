044eea99e084c25815254551f02a5bf68fdf8fcd
Micheal Limones <micheal@zestylimones.com>
2021-11-23 07:56:33 -0700

    added Provider to app.js.

--- client/src/App.js
+++ client/src/App.js
@@ -9,14 +9,14 @@ import {
 import { setContext } from '@apollo/client/link/context';
 
 import store from './utils/store';
-
+import { Provider } from 'react-redux';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
+// import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
@@ -44,7 +44,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider store={store}>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -55,7 +55,7 @@ function App() {
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
